b73d706fbe872272fcf698607ced592ec336d5ee
lno-Mier <108462758+lno-Mier@users.noreply.github.com>
2022-08-23 17:47:22 +0600

    Update Jah.js

--- Jah.js
+++ Jah.js
@@ -1,11 +1,11 @@
 function bingo() {
 	let names = document.getElementsByTagName('textarea')[0]
-	let arr = names.split('textarea')
-	for (let names of let arr){
+	let arr = names.split(', ')
+	for (let names of arr){
 		repeat(Math.floor(Math.random * arr))
 	}
 	}
 	let input = document.getElementsByTagName('input')[0]
-input.onclick = function() {
+	input.onclick = function() {
 	body.input = alert(bingo)
 	}
